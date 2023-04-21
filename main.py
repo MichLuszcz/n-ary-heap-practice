a910bcaf670d4a005f6747be5c8621b8dfefba9d
Michał Łuszczek <michluszcz@gmail.com>
2023-04-21 16:41:53 +0200

    added priting out a full heap and after a few pops

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from heapClass import Heap
 
 
-my_heap = Heap(2)
-my_heap.add(5)
-my_heap.add(8)
-my_heap.add(10)
-my_heap.add(4)
-pass
-print(".")
+my_heap = Heap(3)
+my_heap.add(5, 5)
+my_heap.add(8, 8)
+my_heap.add(10, 10)
+my_heap.add(4, 4)
+my_heap.add(2, 2)
+my_heap.add(1, 1)
+my_heap.add(3, 3)
+my_heap.add(6, 6)
+my_heap.add(7, 7)
+
+print("---" * 15)
+print(my_heap)
+
+print(my_heap.pop())
+print("---" * 15)
+print(my_heap)
+print(my_heap.pop())
+
+print("---" * 15)
+print(my_heap)
+print(my_heap.pop())
+print(my_heap.pop())
+
+print("---" * 15)
+print(my_heap)
